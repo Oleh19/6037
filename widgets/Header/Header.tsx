e41b9c4d7f112bc200feee02621c227ef2f32cc4
Oleh <deckart212@gmail.com>
2026-01-19 05:17:31 +0200

    Refactor Header component to remove title prop and set a static title

--- widgets/Header/Header.tsx
+++ widgets/Header/Header.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import styles from './Header.module.scss';
 
-type HeaderProps = {
-  title?: string;
-};
-
-function Header({ title }: HeaderProps) {
+function Header() {
   return (
     <header className={styles.siteHeader}>
       <span className={styles.siteHeaderTitle}>
-        {title}
+        6037 Venture Partnership
       </span>
     </header>
   );
 }
-
-Header.defaultProps = {
-  title: '6037 Venture Partnership',
-};
 
 export default Header;
